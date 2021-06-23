bdfc877ec8b790d5b48549e1ba4e271561228c73
pithlessly <79002325+pithlessly@users.noreply.github.com>
2021-06-23 12:15:39 -0700

    add basic compiler driver

--- src/Main.hs
+++ src/Main.hs
@@ -5,28 +5,58 @@ import qualified Parse
 import qualified Sema
 import qualified Codegen
 
+import System.Exit (exitWith, ExitCode(ExitFailure))
 import System.IO (hPutStrLn, stderr)
+import System.IO.Error (tryIOError, ioeGetErrorString)
+import System.Environment (getArgs)
 
+import Data.Text (Text)
 import qualified Data.Text as Text
+import qualified Data.Text.IO as Text
 
 
+err :: String -> String -> IO a
+err src msg = do
+  let formatted =
+        case lines msg of
+          [] -> "\ESC[m"
+          [_] -> ":\ESC[m " ++ msg
+          ls -> ":\ESC[m" ++ concatMap ("\n  " ++) ls
+
+  hPutStrLn stderr $ concat ["\ESC[1;31merror (", src, ")", formatted]
+  exitWith $ ExitFailure 1
+
+-- helper to test compilation in the REPL
 compile :: String -> IO ()
 compile s =
   case Parse.parse (Text.pack s) of
     Left msg -> err "parse" msg
     Right ast ->
       case Sema.sema ast of
-        Left msg -> err "semantic analysis" msg
+        Left msg -> err "sema" msg
         Right ir ->
           putStr $ Text.unpack $ Codegen.codegen ir
-  where
-    err src msg =
-      let formatted =
-            case lines msg of
-              [] -> "\ESC[m"
-              [_] -> ":\ESC[m " ++ msg
-              ls -> ":\ESC[m" ++ concatMap ("\n  " ++) ls
-      in hPutStrLn stderr $ concat ["\ESC[1;31m", src, " error", formatted]
 
+-- main compilation driver
 main :: IO ()
-main = return ()
+main =
+  try readCode >>= \(_, code) ->
+    case Parse.parse code of
+      Left msg -> err "parse" msg
+      Right ast ->
+        case Sema.sema ast of
+          Left msg -> err "sema" msg
+          Right ir -> do
+            let asm = Codegen.codegen ir
+            try $ Text.writeFile "out.asm" asm
+            hPutStrLn stderr "finished, all ok."
+  where
+    try :: IO a -> IO a
+    try f = tryIOError f >>= either (err "IO" . ioeGetErrorString) return
+
+    readCode :: IO (FilePath, Text)
+    readCode = getArgs >>= \case
+      [path] -> do
+        code <- Text.readFile path
+        return (path, code)
+      _ -> err "driver" "invalid arguments"
